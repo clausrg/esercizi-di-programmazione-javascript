e3f17fcc50ebaae515f657a48628e2be41fbf780
clausrg <srg@srg.mi.it>
2020-09-17 18:01:08 +0200

    first solution to somma e media

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,14 @@
   
   http://www.imparareaprogrammare.it
 */
+
+/* METODO SEMPLICE*/
+const a = 1, b = 2, c = 3, d = 4, e = 5;
+
+const sum = a+b+c+d+e;
+const avg = (sum)/5;
+
+console.log(`Input: a = ${a}, b = ${b}, c = ${c}, d = ${d}, e = ${e}`);
+console.log(`Output: somma = ${sum}, media = ${avg}`);
+
+/* Prova a utilizzare i tagged template e le array*/
