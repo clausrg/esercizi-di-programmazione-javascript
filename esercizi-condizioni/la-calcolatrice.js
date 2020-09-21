00b2d074d7ca836ebad0104db6ba081d294a63b7
clausrg <srg@srg.mi.it>
2020-09-21 13:59:04 +0200

    first solution to la calcolatrice

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -28,3 +28,44 @@
 
   http://www.imparareaprogrammare.it
 */
+
+const operation = 'somma';
+const a = 6;
+const b = 5;
+
+let output;
+
+switch (operation) {
+  case 'somma' :
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a+b}`;
+  break;
+
+  case 'sottrazione' :
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a-b}`;
+  break;
+
+  case 'moltiplicazione' :
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a*b}`;
+  break;
+
+  case 'divisione' :
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a/b}`;
+  break;
+
+  case 'modulo' :
+  output = `resto della divisione tra ${a} e ${b} (solo tra interi): il risultato è ${Math.floor(a)%Math.floor(b)}`;
+  break;
+
+  case 'potenza' :
+  output = `${operation} di ${a} alla ${b}°: il risultato è ${Math.pow(a, b)}`;
+  break;
+
+  case 'media' :
+  output = `${operation} tra ${a} e ${b}: il risultato è ${(a+b)/2}`;
+  break;
+
+  default :
+  output = `${operation}: operazione non valida`;
+}
+
+console.log(output);
