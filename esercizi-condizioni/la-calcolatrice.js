16112d1bd0c4d046fcb51a64dcbb65e60639bf8f
clausrg <srg@srg.mi.it>
2020-09-21 16:58:38 +0200

    Update la-calcolatrice.js

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -29,39 +29,39 @@
   http://www.imparareaprogrammare.it
 */
 
-const operation = 'somma';
-const a = 6;
-const b = 5;
+const operation = 'potenza';
+const a = 2;
+const b = 3;
 
 let output;
 
 switch (operation) {
   case 'somma' :
-  output = `${operation} tra ${a} e ${b}: il risultato è ${a+b}`;
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a + b}`;
   break;
 
   case 'sottrazione' :
-  output = `${operation} tra ${a} e ${b}: il risultato è ${a-b}`;
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a - b}`;
   break;
 
   case 'moltiplicazione' :
-  output = `${operation} tra ${a} e ${b}: il risultato è ${a*b}`;
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a * b}`;
   break;
 
   case 'divisione' :
-  output = `${operation} tra ${a} e ${b}: il risultato è ${a/b}`;
+  output = `${operation} tra ${a} e ${b}: il risultato è ${a / b}`;
   break;
 
   case 'modulo' :
-  output = `resto della divisione tra ${a} e ${b} (solo tra interi): il risultato è ${Math.floor(a)%Math.floor(b)}`;
+  output = `resto della divisione tra ${a} e ${b} (solo tra interi): il risultato è ${Math.floor(a) % Math.floor(b)}`;
   break;
 
   case 'potenza' :
-  output = `${operation} di ${a} alla ${b}°: il risultato è ${Math.pow(a, b)}`;
+  output = `${operation} di ${a} alla ${b}°: il risultato è ${a ** b}`;
   break;
 
   case 'media' :
-  output = `${operation} tra ${a} e ${b}: il risultato è ${(a+b)/2}`;
+  output = `${operation} tra ${a} e ${b}: il risultato è ${(a + b) / 2}`;
   break;
 
   default :
