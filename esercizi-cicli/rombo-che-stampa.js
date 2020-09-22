c9a31c5f396137c5a26361ddf9c68a63e1df082c
clausrg <srg@srg.mi.it>
2020-09-22 14:26:14 +0200

    first solution to rombo che stampa

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -16,3 +16,37 @@
   
   http://www.imparareaprogrammare.it
 */
+
+
+const n = 17; // solo numeri dispari
+const maxRow = n;
+const minRow = 1;
+const middleRow = (minRow + maxRow) / 2;
+
+const tabs = '\t'+'\t';
+const fullChar = 'X';
+const emptyChar = ' ';
+
+let numFull, numEmpty, full, empty;
+
+let i = minRow;
+
+console.log(`Input: ${n}\nOutput:`);
+
+while (i <= maxRow) {
+  numFull = n - Math.abs(i - middleRow) * 2;
+  numEmpty = (n - numFull) / 2;
+
+  full = '';
+  for (let i1=0; i1<numFull; i1++) {
+    full += fullChar;
+  }
+
+  empty = '';
+  for (let i1=0; i1<numEmpty; i1++) {
+    empty += emptyChar;
+  }
+  console.log(`${i}${tabs}${empty}${full}`);
+
+  i++;
+}
