c91e97850cb0ca754accb872d73f9ed1e5e89127
clausrg <srg@srg.mi.it>
2020-09-25 17:52:44 +0200

    first solution to tanti numeri

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -12,3 +12,13 @@
 
   http://www.imparareaprogrammare.it
 */
+
+const numbers = [3, 5, 10, 2, 8];
+let numSum = 0;
+numbers.forEach((num) => numSum+=num);
+const numAvg = numSum/numbers.length;
+const subAvg = numbers.filter(num => num<numAvg);
+
+console.log(`Input: numbers = ${numbers}`);
+console.log(`Output: media = ${numAvg},
+	valori minori = ${subAvg}`);
