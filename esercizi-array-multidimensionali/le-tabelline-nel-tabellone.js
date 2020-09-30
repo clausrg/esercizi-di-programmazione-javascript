065e82b887932d1e30a1acdec3cc193dee0c34e5
clausrg <srg@srg.mi.it>
2020-09-30 14:37:23 +0200

    first solution to le tabelline nel tabellone

--- esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
+++ esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
@@ -11,3 +11,33 @@
     
   http://www.imparareaprogrammare.it
 */
+
+
+const a = [];
+const rows = 10;
+const cols = 10;
+const minRow = 0;
+const minCol = 0;
+
+for (let x=minRow; x<=rows; x++) {
+  a[x] = [];
+  for (let y=minCol; y<=cols; y++) {
+    a[x][y] = x*y;
+  }
+}
+console.table(a);
+
+/* Stampo esattamente come da esempio */
+
+const sep = '\t|\t';
+const b = [];
+let head = 'x';
+
+for (let i=0; i<=rows; i++) {
+  head += `${sep}${i}`;
+  if (i>=minRow) {
+    b[i] = `${i}${sep}${a[i].join(sep)}`;
+  }
+}
+
+console.log(`${head}\n${b.join('\n')}`);
