cdf8fc0cccc10c8a054963d929bc453f3a82352b
clausrg <srg@srg.mi.it>
2020-09-30 15:45:02 +0200

    first solution to il sottosopra

--- esercizi-array-multidimensionali/il-sottosopra.js
+++ esercizi-array-multidimensionali/il-sottosopra.js
@@ -17,3 +17,40 @@
 
   http://www.imparareaprogrammare.it
 */
+
+const rows = 3;
+const cols = 2;
+
+const max = rows*cols;
+const min = 1;
+
+const a = [];
+const b = [];
+
+for (let x=0; x<rows; x++) {
+  a[x] = [];
+
+  for (let y=0; y<cols; y++) {
+    a[x][y] = (cols*x) + (y+1);
+  }
+}
+
+for (let x=0; x<cols; x++) {
+  b[x] = [];
+
+  for (let y=0; y<rows; y++) {
+    b[x][y] = a[y][x];
+  }
+}
+
+// console.table(a);
+// console.table(b);
+
+console.log(`\tInput:
+  \t\t[
+  \t\t\t[${a.join('],\n\t\t\t[')}]
+  \t\t]
+  \tOutput:
+  \t\t[
+  \t\t\t[${b.join('],\n\t\t\t[')}]
+  \t\t]`);
