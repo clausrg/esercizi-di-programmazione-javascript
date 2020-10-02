3b9fc7c2c79179a8f7d7e6b0e83b82f579f94420
clausrg <srg@srg.mi.it>
2020-10-02 15:06:01 +0200

    first solution to indovina il giorno

--- esercizi-funzioni/indovina-il-gioco.js
+++ esercizi-funzioni/indovina-il-gioco.js
@@ -14,3 +14,40 @@
 
   http://www.imparareaprogrammare.it
 */
+
+function controlNum(n) {
+  return (n>=1)&&(n<=7);
+}
+
+function returnDay(n) {
+  if (controlNum(n)) {
+    switch (n) {
+      case 1:
+      return `${n} = Lunedì`
+
+      case 2:
+      return `${n} = Martedì`
+
+      case 3:
+      return `${n} = Mercoledì`
+
+      case 4:
+      return `${n} = Giovedì`
+
+      case 5:
+      return `${n} = Venerdì`
+
+      case 6:
+      return `${n} = Sabato`
+
+      case 7:
+      return `${n} = Domenica`
+    }
+  } else {
+    return 'Peccato, non posso indovinare il giorno.'
+  }
+}
+
+let num = parseInt(prompt('Inserisci un numero'));
+
+console.log(returnDay(num));
