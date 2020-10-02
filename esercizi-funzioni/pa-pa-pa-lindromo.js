52e467d66fbb5278f76709591ced96c6f48b62a9
clausrg <srg@srg.mi.it>
2020-10-02 18:27:58 +0200

    first solution to pa-pa-pa-lindromo

--- esercizi-funzioni/pa-pa-pa-lindromo.js
+++ esercizi-funzioni/pa-pa-pa-lindromo.js
@@ -14,3 +14,27 @@
 
   http://www.imparareaprogrammare.it
 */
+
+/* Metodo più complesso confronta la prima metà della stringa
+con l'inverso della seconda metà e se sono uguali la stringa iniziale è palindroma */
+
+function checkPal1(str) {
+  const a = str.replace(/\W/g, "");
+  let b = a.slice(0, Math.floor(a.length/2));
+  let c = a.slice(Math.ceil(a.length/2), a.length).split("").reverse().join("");
+
+  return ((b===c) ? 'TRUE' : 'FALSE')
+}
+
+/* Metodo più semplice confronta la stringa con la copia invertita */
+function checkPal2(str) {
+  const a = str.replace(/\W/g, "");
+  let b = a.split("").reverse().join("");
+
+  return ((a===b) ? 'TRUE' : 'FALSE')
+}
+
+const str = 'i topi non avevano nipoti';
+
+console.log(`Input: ${str}
+  Output:${checkPal2(str)}`);
