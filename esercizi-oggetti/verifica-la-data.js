94d0db2003dadfc6b247e2bed1bfacd418838c91
clausrg <srg@srg.mi.it>
2020-10-07 14:42:38 +0200

    first solution to verifica la data

--- esercizi-oggetti/verifica-la-data.js
+++ esercizi-oggetti/verifica-la-data.js
@@ -13,3 +13,22 @@
 
   http://www.imparareaprogrammare.it
 */
+
+function checkDate(d,m,y) {
+  const mDays = [31,29,31,30,31,30,31,31,30,31,30,31];
+  const numDays = mDays[m-1];
+  if ((m>0)&&(m<=12)&&(d>0)&&(d<=numDays)&&(y>0)) {
+    var date = `${d}/${m}/${y}`;
+  } else {
+    var date = 'La data non è valida!';
+  }
+  const printDate = `Input:\n\tday: ${d}\n\tmonth: ${m}\n\tyear: ${y}\nOutput: \n\t${date}`;
+
+  return printDate
+}
+
+const d = prompt('Inserisci un giorno');
+const m = prompt('Inserisci un mese');
+const y = prompt('Inserisci un anno');
+
+console.log(checkDate(d,m,y));
